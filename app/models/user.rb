677411c3af171ccf06ad961c7ff0a3e9d4400905
amrxt1 <amrxt11@outlook.com>
2025-04-07 10:42:19 -0700

    Gatekeep website to CC@Students

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,16 @@ class User < ActiveRecord::Base
   extend Devise::Models
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  validates :username, presence: true, uniqueness: { case_sensitive: true }
+  before_validation :set_default_username, on: :create
+
+  # validations
   validates :last_name, presence: true
   validates :first_name, presence: true
+  validates :username, presence: true, uniqueness: { case_sensitive: true }
+  validate :must_be_columbia_student_email, on: :create
 
   has_many :listings, dependent: :destroy
   has_one :tutor_profile, dependent: :destroy
@@ -20,4 +24,17 @@ class User < ActiveRecord::Base
   has_many :study_groups, through: :study_group_memberships, dependent: :destroy
 
   has_many :messages, dependent: :destroy
+
+  private
+
+  def must_be_columbia_student_email
+    unless email.match?(/\A\d+@columbiacollege\.ca\z/)
+      errors.add(:email, "must be a valid Columbia College student email")
+    end
+  end
+  def set_default_username
+    if username.blank? && email.match?(/\A\d+@columbiacollege\.ca\z/)
+      self.username = email.split("@").first
+    end
+  end
 end
